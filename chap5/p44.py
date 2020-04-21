928c426c284e99876308b7b30d2328efebf1ce2d
氏家翔吾 <suzzz428@gmail.com>
2020-04-21 22:38:02 +0900

    p44

---
+++ chap5/p44.py
@@ -0,0 +1,17 @@
+from p41 import create_chunks
+import pydot
+
+if __name__ == '__main__':
+    with open('neko.txt.cabocha', 'r') as f:
+        lines = [line.split('\n') for line in f.read().split('EOS\n') if line != '']
+
+    lines = [[l.replace('\t', ',').split(',') for l in line if l != ''] for line in lines]
+
+    edges = []
+    sents = create_chunks(lines)
+    for c in sents[10]:
+        if c.dst != -1:
+            edges.append((str(c), str(sents[10][c.dst])))
+
+    g = pydot.graph_from_edges(edges,directed=True)
+    g.write_png("dependenttree.png")
