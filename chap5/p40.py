c132a672d45535e31c48b43269c7316823327c92
氏家翔吾 <suzzz428@gmail.com>
2020-04-21 21:11:33 +0900

    add p40

--- chap5/p40.py
+++ chap5/p40.py
@@ -1,4 +1,17 @@
+import unicodedata
+
+class Morph(object):
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
 if __name__ == '__main__':
     with open('neko.txt.cabocha', 'r') as f:
         lines = [line.split('\n') for line in f.read().split('EOS\n') if line != '']
-    print(lines)
+
+    lines = [[l.replace('\t', ',').split(',') for l in line if l != ''] for line in lines]
+    lines = [[Morph(l[0], l[7], l[1], l[2]) for l in line if l[0][0] != '*'] for line in lines]
+
+    print(lines[2])
