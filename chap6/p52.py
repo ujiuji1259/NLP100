56aea0e80786c0f444c84d7a0dfb06ad61dd16e7
ujiuji1259 <suzzz428@gmail.com>
2021-04-13 02:22:23 +0900

    add p52

---
+++ chap6/p52.py
@@ -0,0 +1,34 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+import csv
+import numpy as np
+
+def load_dataset(path):
+    datasets = []
+    with open(path, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            datasets.append(row)
+
+    return datasets
+
+def preprocess(data):
+    x = [d[1] for d in data]
+    y = [d[4] for d in data]
+
+    x = [d.lower() for d in x]
+    return x, y
+
+train_data = load_dataset("NewsAggregatorDataset/train.txt")
+valid_data = load_dataset("NewsAggregatorDataset/valid.txt")
+test_data = load_dataset("NewsAggregatorDataset/test.txt")
+train_x, train_y = preprocess(train_data)
+valid_x, valid_y = preprocess(valid_data)
+test_x, test_y = preprocess(test_data)
+
+model = CountVectorizer()
+train_x = model.fit_transform(train_x)
+valid_x = model.transform(valid_x)
+test_x = model.transform(test_x)
+
+clf = LogisticRegression(random_state=0).fit(train_x, train_y)
